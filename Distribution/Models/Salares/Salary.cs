bf459bde92ea9122f004cbb1afef1f053aa34003
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-28 16:32:33 +0500

    DATA: Model & Migrate Salary

---
+++ Distribution/Models/Salares/Salary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Distribution.Models.Salares
+{
+    public class Salary
+    {
+        public Guid id { get; set; }
+        public decimal Amount { get; set; }
+        public int Experience { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public Guid Locationld { get; set; }
+        public Guid Languageld { get; set; }
+        public Guid Jobld { get; set; }
+    }
+}
