2dd19ca52ab3d04a20ac7f64957d74848ac9ecca
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-02-28 14:57:17 +0500

    INFRA: Initialize Api Project

---
+++ Distribution/Controllers/DelateMe.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Distribution.Controllers
+{
+    [ApiController]
+    [Route("api/Controller")]
+    public class DelateMe : ControllerBase
+    {
+        [HttpGet]
+        public string Get() => "Hello";
+
+    }
+}
