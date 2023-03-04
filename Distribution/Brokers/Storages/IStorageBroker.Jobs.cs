8f854e86984fee529b57e9203ac799ed01bbcaa6
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-03-04 23:19:11 +0500

    BROKERS: Insert Job

---
+++ Distribution/Brokers/Storages/IStorageBroker.Jobs.cs
@@ -0,0 +1,11 @@
+﻿using Distribution.Models.Jobs;
+using Distribution.Models.Locations;
+using System.Threading.Tasks;
+
+namespace Distribution.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Job> InsertJobAsync(Job job);
+    }
+}
